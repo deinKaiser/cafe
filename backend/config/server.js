7e5b49471c6ba4003150109be53555a38b4b77fe
Kaiser <yarema.vadum@gmail.com>
2021-06-14 01:10:46 +0300

    commit for idiots

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07c9dd7a6323322a72617dcab3818737'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1e49d39b-636e-47de-ae34-6cd202aee383'
+};
